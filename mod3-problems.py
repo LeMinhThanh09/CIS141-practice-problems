85d75c80ceeb134c0cba974da7e809bba34229a9
LeMinhThanh <108313258+LeMinhThanh09@users.noreply.github.com>
2025-01-27 22:55:19 -0800

    Create mod3-problems.py

---
+++ mod3-problems.py
@@ -0,0 +1,9 @@
+# 1. Prompt the user for a word. Then, prompt the user for how many times they'd like that word repeated. Use the skills you learned in this module to print the word the correct number of times.
+
+#2. Prompt the user for their name and their age. Calculate their age next year. Use string concatenation to print "Hello, <name>! You are <age1> years old. Next year, you will be <age2> years old."
+
+#3. Prompt the user for a sentence and a word to try to find in that sentence. Have the program print out whether the word was found in the sentence. (i.e. True or False)
+
+#4. Prompt the user for: a word, a first index, and a last index. Slice the word at the indexes provided by the user and print to the screen.
+
+#5. Print 3 words with a | character (called a pipe) in between them. Use the appropriate keyword argument in print() to do so.
